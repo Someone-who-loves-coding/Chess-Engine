919471a929e064e5c75f06264c1a39494b2cd6de
viditpokhral <128287510+viditpokhral@users.noreply.github.com>
2023-10-02 21:01:17 +0530

    scripts.js

--- Frontend/WebPages/scripts.js
+++ Frontend/WebPages/scripts.js
@@ -1,1 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const chessboard = document.getElementById("chessboard");
+    let isWhite = true;
+
+    for (let row = 0; row < 8; row++) {
+        for (let col = 0; col < 8; col++) {
+            const tile = document.createElement("div");
+            tile.className = isWhite ? "white" : "black";
+            chessboard.appendChild(tile);
+            isWhite = !isWhite;
+        }
+        isWhite = !isWhite; // Switch color for each row
+    }
+});
 
